fb553e3d8ef497d026ba12dc174936936381db00
Michal Rowicki <Michal.Rowicki@vimn.com>
2020-05-14 09:35:30 +0200

    SpringApplicationBuilder event listener

--- src/main/java/tech/viacomcbs/intothespring/IntoTheSpringApplication.java
+++ src/main/java/tech/viacomcbs/intothespring/IntoTheSpringApplication.java
@@ -1,14 +1,14 @@
 package tech.viacomcbs.intothespring;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class IntoTheSpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(
-            new Class<?>[]{IntoTheSpringApplication.class},
-            args);
+        new SpringApplicationBuilder(IntoTheSpringApplication.class)
+            .listeners(new SheldonApplicationListener())
+            .run(args);
     }
 }
